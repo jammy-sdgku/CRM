4bfcf09125b988fc02c593113e82e5b94ac4e900
jammy-sdgku <james.jones@sdgku.edu>
2026-01-12 17:50:41 +0000

    created login, logout and db

--- website/views.py
+++ website/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+
+def home(request):
+    if request.method == 'POST':
+        username = request.POST['userName']
+        password = request.POST['password']
+        #authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Your login is successful!')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password.')
+            return redirect('home')
+    else:   
+        return render(request, 'home.html', {}) 
+
+
+def logout_user(request):
+    logout(request)
+    messages.info(request, 'You have been logged out...')
+    return redirect('home')
