f2a6a99251c24513ab5cdbf183496df57bf80719
jammy-sdgku <james.jones@sdgku.edu>
2026-01-15 15:59:05 +0100

    Added models

--- website/models.py
+++ website/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=15)
+    email = models.EmailField(unique=True)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
